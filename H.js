3fac62fe435ff027f06737f7365a8d526b2e0a87
Yassine Bouanane <yassine@bouanane.com>
2019-12-25 10:42:46 -0500

    readFile now returns file as string by default

--- H.js
+++ H.js
@@ -9,7 +9,8 @@ const child_process = require('child_process');
 
 
 // File / system
-module.exports.readFile = util.promisify(fs.readFile);
+module.exports.readFile = async () => util.promisify(fs.readFile)(...arguments).toString();
+module.exports.readFileBuff = util.promisify(fs.readFile);
 module.exports.readDir = util.promisify(fs.readdir);
 module.exports.writeFile = util.promisify(fs.writeFile);
 
